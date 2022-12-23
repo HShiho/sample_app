e2da9c3a93881a30d238ca0654f923c422a63dcb
HShiho <108495008+HShiho@users.noreply.github.com>
2022-12-23 12:10:37 +0000

    [add]AllLists作成計画リスト

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root :to => 'homes#top'
 
   resources :lists
+  
+  get 'home/all_lists' => 'homes#test',as: 'all_lists'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
